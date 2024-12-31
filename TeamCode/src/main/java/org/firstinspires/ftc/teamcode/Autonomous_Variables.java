2539cfdbeb0ec0f766c8e0a37bbed2347566d5e9
Rusty-Nail-Robotics <Team@ftc18045.com>
2024-12-30 21:31:22 -0700

    started auto program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Variables.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Variables.java
@@ -10,13 +10,16 @@ public class Autonomous_Variables {
     public static double[] sub_Spec_Set_Location = {-6,-36,90};
     public static int subDeliverLiftTarget = 1650;
     public static int subDeliverLondonTarget = 700;
-    public static int subDeliverLondonDropDistance = 100;
+    public static int subDeliverLondonDropDistance = 120;
     public static int subDeliverRetractionDistance = 900;
 
 
-    public static double[] pick_From_Ground_Inner_Location = {-46, -40, 90};
+    public static double[] pick_From_Ground_Inner_Location = {-46, -41, 90};
+    public static double[] pick_From_Ground_Center_Location ={-55,-41,90};
+    public static double[] pick_From_Ground_Outer_Location = {-48,-22,180};
     public static int pickFromGroundLondonTarget = -100;
     public static int pickFromGroundLiftTarget = 1650;
 
+    public static double[] safe_Pivot_Point = {-46,-46};
     public static double[] place_In_Basket_Location = {-48,-48,225};
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Basket_Side_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Basket_Side_Auto.java
@@ -7,71 +7,64 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.MecanumDrive;
-import org.firstinspires.ftc.teamcode.TankDrive;
+
 
 @Autonomous(name = "Basket Side")//Set Program Name and Mode
 public final class Basket_Side_Auto extends LinearOpMode {
+    Non_Move non_move = new Non_Move();
+    ElapsedTime gameTime = new ElapsedTime();
+    ElapsedTime gpTimer = new ElapsedTime();
+
 
     @Override
 
     public void runOpMode() throws InterruptedException {
         Pose2d beginPose = new Pose2d(-36, -60, Math.toRadians(90));
 
-        ElapsedTime gameTime = new ElapsedTime();
-        ElapsedTime gpTimer = new ElapsedTime();
 
-        Non_Move non_move = new Non_Move();
-        non_move.Setup(hardwareMap,this);               //run the setup function in Non_Move
+
+
+        non_move.Setup(hardwareMap, this);               //run the setup function in Non_Move
 
         Show_Off showOff = new Show_Off();                          //Create the container named showoff
         showOff.Setup(hardwareMap, this);               //run the setup function in Show_Off
 
         MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
         Action StartToSub = drive.actionBuilder(drive.pose)
-                //.lineTo(new Vector2d(0, 0), Math.toRadians(90))
-                //.splineToConstantHeading(new Vector2d(0, 0), Math.toRadians(90))
                 .strafeToLinearHeading(new Vector2d(Autonomous_Variables.sub_Spec_Set_Location[0], Autonomous_Variables.sub_Spec_Set_Location[1]), Math.toRadians(Autonomous_Variables.sub_Spec_Set_Location[2]))
                 .build();
-            non_move.londonMotor.setPower(.5);
+        non_move.londonMotor.setPower(.5);
 
         waitForStart();
         gameTime.reset();
 
+            //// Place on High Chamber//
+        Global_Variables.basketMode = 3;
+        non_move.AutoLiftOperations(this);
+        Actions.runBlocking(
+                new SequentialAction(
+                        StartToSub
+                )
+        );
 
-            Global_Variables.basketMode = 3;
-            non_move.AutoLiftOperations(this);
-            Actions.runBlocking(
-                    new SequentialAction(
-                            StartToSub
-                    )
-            );
-        gpTimer.reset();
-        while(gpTimer.seconds() < .5  && !isStopRequested()){
-            //Do Nothing
-        }
-        non_move.londonMotor.setTargetPosition(Autonomous_Variables.subDeliverLondonTarget- Autonomous_Variables.subDeliverLondonDropDistance);
+        non_move.londonMotor.setTargetPosition(Autonomous_Variables.subDeliverLondonTarget - Autonomous_Variables.subDeliverLondonDropDistance);
+        WaitSeconds(.5);
         non_move.liftMotor.setTargetPosition(Autonomous_Variables.subDeliverLiftTarget - Autonomous_Variables.subDeliverRetractionDistance);
 
-        gpTimer.reset();
-        while(gpTimer.seconds() < .5  && !isStopRequested()){
-                //Do Nothing
-            }
+        WaitSeconds(.75);
 
         non_move.gripServo.setPosition(Global_Variables.gripperOpen);
 
-        gpTimer.reset();
-        while(gpTimer.seconds() < .5 && !isStopRequested()){
-                //Do Nothing
-            }
+        WaitSeconds(.15);
 
-            Global_Variables.basketMode = 2;
-            non_move.AutoLiftOperations(this);
+        //Pick up Inner Sample//
 
-            /// Pick From Floor
+        Global_Variables.basketMode = 2;
+        non_move.AutoLiftOperations(this);
+
         Action PickInnerFromFloor = drive.actionBuilder(drive.pose)
                 .strafeToLinearHeading(new Vector2d(Autonomous_Variables.pick_From_Ground_Inner_Location[0], Autonomous_Variables.pick_From_Ground_Inner_Location[1]), Math.toRadians(Autonomous_Variables.pick_From_Ground_Inner_Location[2]))
                 .build();
@@ -82,23 +75,21 @@ public final class Basket_Side_Auto extends LinearOpMode {
         );
 
         non_move.liftMotor.setTargetPosition(Autonomous_Variables.pickFromGroundLiftTarget);
-        while(non_move.liftMotor.getCurrentPosition()<900){DoNothing();}
-        non_move.londonMotor.setTargetPosition(Autonomous_Variables.pickFromGroundLondonTarget);
-        gpTimer.reset();
-        while(gpTimer.seconds() < .5 && !isStopRequested()){
-            //Do Nothing
+        while (non_move.liftMotor.getCurrentPosition() < 900) {
+            telemetry.update();//do nothing
         }
+        non_move.londonMotor.setTargetPosition(Autonomous_Variables.pickFromGroundLondonTarget);
+        WaitSeconds(1);
         non_move.gripServo.setPosition(Global_Variables.gripperClosed);
 
+        //Place Inner Sample in High Basket
 
-        gpTimer.reset();
-        while(gpTimer.seconds() < .5 && !isStopRequested()){
-            //Do Nothing
-        }
-
-        non_move.londonMotor.setTargetPosition(30);
+        WaitSeconds(.5);
+        non_move.londonMotor.setPower(1);
+        non_move.londonMotor.setTargetPosition(Global_Variables.basketLondonTarget);
 
         Action PrepPlaceInBasket = drive.actionBuilder(drive.pose)
+                .strafeTo(new Vector2d(Autonomous_Variables.safe_Pivot_Point[0],Autonomous_Variables.safe_Pivot_Point[1]))
                 .turnTo(Math.toRadians(225))
                 .build();
         Actions.runBlocking(
@@ -106,8 +97,8 @@ public final class Basket_Side_Auto extends LinearOpMode {
                         PrepPlaceInBasket
                 )
         );
-        Global_Variables.basketMode=1;
-        while(non_move.liftMotor.getCurrentPosition() < Global_Variables.maxViperUnderMinBeforeLift + 100){
+        Global_Variables.basketMode = 1;
+        while (non_move.liftMotor.getCurrentPosition() < Global_Variables.maxViperUnderMinBeforeLift + 100) {
             non_move.AutoLiftOperations(this);
         }
         Action PlaceInBasket = drive.actionBuilder(drive.pose)
@@ -120,22 +111,79 @@ public final class Basket_Side_Auto extends LinearOpMode {
         );
         non_move.gripServo.setPosition(Global_Variables.gripperOpen);
 
+//Pick Middle Sample from floor
 
+        Action PickCenterFromFloor = drive.actionBuilder(drive.pose)
+                .strafeTo(new Vector2d(Autonomous_Variables.safe_Pivot_Point[0],Autonomous_Variables.safe_Pivot_Point[1]))
+                .afterDisp(0, RetractLift())
+                .strafeToLinearHeading(new Vector2d(Autonomous_Variables.pick_From_Ground_Center_Location[0], Autonomous_Variables.pick_From_Ground_Center_Location[1]), Math.toRadians(Autonomous_Variables.pick_From_Ground_Center_Location[2]))
 
+                .build();
+        Actions.runBlocking(
+                new SequentialAction(
+                        PickCenterFromFloor
+                )
+        );
+        non_move.liftMotor.setTargetPosition(Autonomous_Variables.pickFromGroundLiftTarget);
 
-        gpTimer.reset();
-        while(gpTimer.seconds() < 2 && !isStopRequested()){
-            //Do Nothing
+        WaitSeconds(2);
+        non_move.londonMotor.setPower(.35);
+        non_move.londonMotor.setTargetPosition(Autonomous_Variables.pickFromGroundLondonTarget);
+        WaitSeconds(1.5);
+        non_move.gripServo.setPosition(Global_Variables.gripperClosed);
+        WaitSeconds(.5);
+
+        //Place middle sample in High Basket
+
+        non_move.londonMotor.setPower(1);
+        non_move.londonMotor.setTargetPosition(Global_Variables.basketLondonTarget);
+
+        Action PrepPlaceInBasketCent = drive.actionBuilder(drive.pose)
+                .strafeTo(new Vector2d(Autonomous_Variables.safe_Pivot_Point[0],Autonomous_Variables.safe_Pivot_Point[1]))
+                .turnTo(Math.toRadians(225))
+                .build();
+        Actions.runBlocking(
+                new SequentialAction(
+                        PrepPlaceInBasketCent
+                )
+        );
+        Global_Variables.basketMode = 1;
+        while (non_move.liftMotor.getCurrentPosition() < Global_Variables.maxViperUnderMinBeforeLift + 100) {
+            non_move.AutoLiftOperations(this);
         }
+        Action PlaceInBasketCent = drive.actionBuilder(drive.pose)
+                .strafeToLinearHeading(new Vector2d(Autonomous_Variables.place_In_Basket_Location[0], Autonomous_Variables.place_In_Basket_Location[1]), Math.toRadians(Autonomous_Variables.place_In_Basket_Location[2]))
+                .build();
+        Actions.runBlocking(
+                new SequentialAction(
+                        PlaceInBasketCent
+                )
+        );
+        non_move.gripServo.setPosition(Global_Variables.gripperOpen);
 
 
+        // Pick Outer Sample from floor
 
+        Action PickOuterFromFloor = drive.actionBuilder(drive.pose)
+                .strafeTo(new Vector2d(Autonomous_Variables.safe_Pivot_Point[0],Autonomous_Variables.safe_Pivot_Point[1]))
+                .afterDisp(0, RetractLift())
+                .strafeToLinearHeading(new Vector2d(Autonomous_Variables.pick_From_Ground_Outer_Location[0], Autonomous_Variables.pick_From_Ground_Outer_Location[1]), Math.toRadians(Autonomous_Variables.pick_From_Ground_Outer_Location[2]))
 
-
-
-
-
-
+                .build();
+        Actions.runBlocking(
+                new SequentialAction(
+                        PickOuterFromFloor
+                )
+        );
+        non_move.liftMotor.setTargetPosition(Autonomous_Variables.pickFromGroundLiftTarget);
+        WaitSeconds(2);
+        non_move.londonMotor.setPower(.35);
+        non_move.londonMotor.setTargetPosition(Autonomous_Variables.pickFromGroundLondonTarget);
+        WaitSeconds(1.5);
+        non_move.gripServo.setPosition(Global_Variables.gripperClosed);
+        WaitSeconds(.5);
+        non_move.londonMotor.setPower(1);
+        non_move.londonMotor.setTargetPosition(Global_Variables.basketLondonTarget);
 
 
 
@@ -144,22 +192,36 @@ public final class Basket_Side_Auto extends LinearOpMode {
 
 
         gpTimer.reset();
-            while(gpTimer.seconds() < 10 && !isStopRequested()){
-                telemetry.addData("Waiting ", gpTimer.seconds());
-                telemetry.update();
-            }
-            non_move.londonMotor.setTargetPosition(50);
-            gpTimer.reset();
-        while(gpTimer.seconds() < 2 && !isStopRequested()){
+        while (gpTimer.seconds() < 10 && !isStopRequested()) {
             telemetry.addData("Waiting ", gpTimer.seconds());
             telemetry.update();
         }
-            non_move.ParkLondon();
+        non_move.londonMotor.setTargetPosition(50);
+        gpTimer.reset();
+        while (gpTimer.seconds() < 2 && !isStopRequested()) {
+            telemetry.addData("Waiting ", gpTimer.seconds());
+            telemetry.update();
+        }
+        non_move.ParkLondon();
 
-            }
+    }
 
-            public void DoNothing(){
 
-            }
+    public void WaitSeconds(double seconds) {
+        gpTimer.reset();
+        while (gpTimer.seconds() < seconds && !isStopRequested()) {
+            telemetry.update();
+            //Do Nothing
+        }
+    }
+
+    Action RetractLift(){
+        non_move.liftMotor.setTargetPosition(Autonomous_Variables.pickFromGroundLiftTarget);
+        return RetractLift();
+    }
+
+
 }
+
+
 
